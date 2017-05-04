b71e5353e1669e53606c70af695279e3e8783dcf
Ercan Ucan <ercanucan@gmail.com>
2017-05-04 15:10:38 +0200

    scion coord addr

--- ad_manager/util/defines.py
+++ ad_manager/util/defines.py
@@ -9,7 +9,7 @@ DEFAULT_BANDWIDTH = 1000
 SCION_SUGGESTED_PORT = 31000
 
 # Values related to the SCION coordination service API
-COORD_SERVICE_URI = "http://127.0.0.1:8080"
+COORD_SERVICE_URI = "https://coord.scionproto.net"
 UPLOAD_JOIN_REQUEST_SVC = "/api/as/uploadJoinRequest/"
 UPLOAD_JOIN_REPLY_SVC = "/api/as/uploadJoinReply/"
 POLL_JOIN_REPLY_SVC = "/api/as/pollJoinReply/"
